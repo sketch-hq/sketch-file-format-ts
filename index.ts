0ffcd21e4a870bbd35281b25e4b6ee28ea9ffd93
jedrichards <jedrichards@gmail.com>
2020-06-02 13:05:14 +0100

    include .js extension for deno support

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import * as FileFormat1 from './v1-types'
-import * as FileFormat2 from './v2-types'
-import * as FileFormat3 from './v3-types'
+import * as FileFormat1 from './v1-types.js'
+import * as FileFormat2 from './v2-types.js'
+import * as FileFormat3 from './v3-types.js'
 
 export { FileFormat1, FileFormat2, FileFormat3 }
 
